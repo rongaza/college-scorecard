fae76bcf73268577b49e0f32864ca423778da7f0
Ron Gaza <rongaza@gmail.com>
2020-07-14 13:15:42 -0700

    created intial components and basic layout for  Searcbar, Dashboard, CollegeDisplay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Searchbar from './components/Searchbar';
+import Dashboard from './components/Dashboard';
 
 function App() {
 	return (
 		<div className="App">
-			<header></header>
 			<div>
 				<h2>College Scorecard API</h2>
+				<Searchbar />
+				<Dashboard />
 			</div>
 		</div>
 	);
