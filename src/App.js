74c4293d36fb0c5fba6823c4be3081b6306bd447
Ron Gaza <rongaza@gmail.com>
2020-07-14 15:56:13 -0700

    created api call to fetch data and wired up components to display data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Searchbar from './components/Searchbar';
 import Dashboard from './components/Dashboard';
+import { fetchData } from './Api/api';
 
 function App() {
+	const [error, setError] = useState({});
+	const [collegeData, setCollegeData] = useState([]);
+
+	// get initial data when component first loads
+	useEffect(() => {
+		fetchData('ID', setCollegeData, setError);
+	}, []);
+
 	return (
 		<div className="App">
 			<div>
 				<h2>College Scorecard API</h2>
 				<Searchbar />
-				<Dashboard />
+				<Dashboard collegeData={collegeData} greeting={'hi'} />
 			</div>
 		</div>
 	);
